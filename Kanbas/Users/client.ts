9ee9e022149d0663c403922fd2cb7319eb4354c4
James Quinlivan <jq1187@gmail.com>
2024-04-19 19:48:20 -0400

    fixes

--- Kanbas/Users/client.ts
+++ Kanbas/Users/client.ts
@@ -1,9 +1,3 @@
-import axios from "axios";
-export const USERS_API = process.env.REACT_APP_API_URL;
-export const signin = async (user) => { };
-export const profile = async () => {
-  const response = await axios.post(`${USERS_API}/profile`);
-  return response.data;
-};
+
 
   
